acb388ddc2dade9e1f27a586089700029fa7fac9
Duarte Pereira <dg.pereira@campus.fct.unl.pt>
2022-10-10 03:35:35 +0100

    dependency graph builder function done

--- builder/controller.go
+++ builder/controller.go
@@ -1,6 +1,7 @@
 package builder
 
 import (
+	"cpl_go_proj22/dependency_graph"
 	"cpl_go_proj22/parser"
 )
 
@@ -18,21 +19,32 @@ type Msg struct {
 
 func MakeController(file *parser.DepFile) chan *Msg {
 	reqCh := make(chan *Msg)
-	rootCh := make(chan *Msg)
-	var leafCh chan interface{} //TODO: You may want to change this type
+	//rootCh := make(chan *Msg)
+	//var leafCh chan interface{} //TODO: You may want to change this type
 	// TODO: Startup system that emits outcome of build on rootCh, triggered by an output on leafCh
-	go func() {
-		<-reqCh
-		leafCh <- nil
-		m := <-rootCh
-		switch m.Type {
-		case BuildSuccess:
-			reqCh <- m
-			break
-		case BuildError:
-			reqCh <- m
-			break
-		}
-	}()
+	//go func() {
+	//	<-reqCh
+	//	leafCh <- nil
+	//	m := <-rootCh
+	//	switch m.Type {
+	//	case BuildSuccess:
+	//		reqCh <- m
+	//		break
+	//	case BuildError:
+	//		reqCh <- m
+	//		break
+	//	}
+	//}()
+	makeGraph(file)
 	return reqCh
+}
+
+func makeGraph(file *parser.DepFile) map[string][]string {
+	graph := dependency_graph.NewGraph()
+	for _, rule := range file.Rules {
+		for _, dep := range rule.Deps {
+			graph = dependency_graph.AddToGraph(graph, rule.Object, dep)
+		}
+	}
+	return graph
 }
--- dependency_graph/dependency_graph.go
+++ dependency_graph/dependency_graph.go
@@ -1,20 +1,27 @@
 package dependency_graph
 
-var graph map[string][]string
-
-func init() {
-	graph = make(map[string][]string)
+type Graph interface {
+	AddToGraph() map[string][]string
+	GetDeps() []string
+	GetTargets() []string
+	ClearGraph()
+	GetGraphSize() int
 }
 
-func AddToGraph(target string, dep string) {
+func NewGraph() map[string][]string {
+	return make(map[string][]string)
+}
+
+func AddToGraph(graph map[string][]string, target string, dep string) map[string][]string {
 	graph[target] = append(graph[target], dep)
+	return graph
 }
 
-func GetDeps(target string) []string {
+func GetDeps(graph map[string][]string, target string) []string {
 	return graph[target]
 }
 
-func GetTargets() []string {
+func GetTargets(graph map[string][]string) []string {
 	var targets []string
 	for target := range graph {
 		targets = append(targets, target)
@@ -22,10 +29,6 @@ func GetTargets() []string {
 	return targets
 }
 
-func ClearGraph() {
-	graph = make(map[string][]string)
-}
-
-func GetGraphSize() int {
+func GetGraphSize(graph map[string][]string) int {
 	return len(graph)
 }
